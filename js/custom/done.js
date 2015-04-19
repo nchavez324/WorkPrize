a66d4502d01ea3d3f5a87df6ab4348f412eec3ba
Nick Chavez <nchavez324@yahoo.com>
2015-04-19 14:16:58 -0400

    linked with dynamic feed and request

--- js/custom/done.js
+++ js/custom/done.js
@@ -170,3 +170,62 @@ function dismissLoad() {
 	notDoneDialog.popup("close");
 }
 
+/**
+
+***
+**/
+function taskdetail(link) {
+
+    var $link = $(link)
+    var $profile_img = $link.find('.user-face')
+    if (!$profile_img) return;
+
+    var img_path = $profile_img.attr('src');
+
+    var $profile_name = $link.find('.profile_name');
+
+    var name = $profile_name.html();
+
+    data = {'profile_img': img_path, 'profile_name': name};
+
+    link = "taskrequest.html?" + encodeQueryData(data);
+
+		jQuery.mobile.changePage(link)
+}
+// Usage:
+//   var data = { 'first name': 'George', 'last name': 'Jetson', 'age': 110 };
+//   var querystring = encodeQueryData(data);
+// 
+function encodeQueryData(data) {
+   var ret = [];
+   for (var d in data)
+      ret.push(encodeURIComponent(d) + "=" + encodeURIComponent(data[d]));
+   return ret.join("&");
+}
+
+$(document).on("pagebeforeshow", function(event) {
+
+	var activePage = $.mobile.activePage.attr("id");
+	
+	if(activePage == 'request-page') {
+
+		data = window.location.href.split('?');
+
+		if (data.length < 2) return;
+
+		data_split = data[1].split('&');
+		data = {}
+		for (var i = data_split.length - 1; i >= 0; i--) {
+			s = data_split[i].split('=');
+			data[s[0]] = decodeURIComponent(s[1]);
+		};
+
+		var $profile_img = $(document).find('#request-user-face');
+		var $profile_name = $(document).find('#request-profile-name');
+
+		$profile_img.attr('src', data['profile_img']);
+		$profile_name.html(data['profile_name']);
+	}
+});
+
+
